4107a6e827c28995291a683c7234ce14db6a705f
tharvik <tharvik@gmail.com>
2013-02-19 14:56:24 +0100

    Use java formatter

--- src/ch/epfl/flamemaker/geometry2d/AffineTransformation.java
+++ src/ch/epfl/flamemaker/geometry2d/AffineTransformation.java
@@ -2,9 +2,11 @@ package ch.epfl.flamemaker.geometry2d;
 
 public class AffineTransformation implements Transformation {
 	final private double a, b, c, d, e, f;
-	static final AffineTransformation IDENTITY = new AffineTransformation(1, 0, 0, 0, 1, 0);
-	
-	public AffineTransformation(double a, double b, double c, double d, double e, double f) {
+	public static final AffineTransformation IDENTITY = new AffineTransformation(1, 0,
+			0, 0, 1, 0);
+
+	public AffineTransformation(double a, double b, double c, double d,
+			double e, double f) {
 		this.a = a;
 		this.b = b;
 		this.c = c;
@@ -13,52 +15,52 @@ public class AffineTransformation implements Transformation {
 		this.f = f;
 	}
 
-	public static AffineTransformation newTranslation (double dx, double dy) {
+	public static AffineTransformation newTranslation(double dx, double dy) {
 		return new AffineTransformation(1, 0, dx, 0, 1, dy);
 	}
-	
-	public static AffineTransformation newRotation (double theta) {
+
+	public static AffineTransformation newRotation(double theta) {
 		double sin = Math.sin(theta);
 		double cos = Math.cos(theta);
-		
+
 		return new AffineTransformation(cos, -sin, 0.0, sin, cos, 0.0);
 	}
-	
-	public static AffineTransformation newScaling (double sx, double sy) {
+
+	public static AffineTransformation newScaling(double sx, double sy) {
 		return new AffineTransformation(sx, 0.0, 0.0, 0.0, sy, 0.0);
 	}
-	
-	public static AffineTransformation newShearX (double sx) {
+
+	public static AffineTransformation newShearX(double sx) {
 		return new AffineTransformation(1, sx, 0, 0, 1, 0);
 	}
-	
-	public static AffineTransformation newShearY (double sy) {
+
+	public static AffineTransformation newShearY(double sy) {
 		return new AffineTransformation(1, 0, 0, sy, 1, 0);
 	}
 
-	public Point transformPoint (Point p) {
-		double newx = p.x()*a + p.y()*b + c;
-		double newy = p.x()*d + p.y()*e + f;
-		
+	public Point transformPoint(Point p) {
+		double newx = p.x() * a + p.y() * b + c;
+		double newy = p.x() * d + p.y() * e + f;
+
 		return new Point(newx, newy);
 	}
-	
+
 	public double translationX() {
 		return c;
 	}
-	
+
 	public double translationY() {
 		return f;
 	}
-	
-	public AffineTransformation composeWith(AffineTransformation that) {
-		double newa = a*that.a + b*that.d;
-		double newb = a*that.b + b*that.e;
-		double newc = a*that.c + b*that.f + c;
-		double newd = d*that.a + e*that.d;
-		double newe = d*that.b + e*that.e;
-		double newf = d*that.c + e*that.f + f;
 
-		return new AffineTransformation(newa, newb, newc, newd, newe, newf);
+	public AffineTransformation composeWith(AffineTransformation that) {
+		double newA = this.a * that.a + this.b * that.d;
+		double newB = this.a * that.b + this.b * that.e;
+		double newC = this.a * that.c + this.b * that.f + this.c;
+		double newD = this.d * that.a + this.e * that.d;
+		double newE = this.d * that.b + this.e * that.e;
+		double newF = this.d * that.c + this.e * that.f + this.f;
+
+		return new AffineTransformation(newA, newB, newC, newD, newE, newF);
 	}
 }
