c81b37fb395e2d1b4064508cc4e646a666a2027d
tharvik <tharvik@gmail.com>
2013-02-28 12:21:39 +0100

    Add skel for tests

---
+++ src/ch/epfl/flamemaker/flame/FlameTest.java
@@ -0,0 +1,19 @@
+package ch.epfl.flamemaker.flame;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FlameTest {
+
+	@Test
+	public void testFlame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCompute() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/ch/epfl/flamemaker/flame/VariationTest.java
@@ -0,0 +1,24 @@
+package ch.epfl.flamemaker.flame;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class VariationTest {
+
+	@Test
+	public void testName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIndex() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTransformPoint() {
+		fail("Not yet implemented");
+	}
+
+}
