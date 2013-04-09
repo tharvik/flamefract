1cfa76ed7ff7de51d2c6200cf29fb8a96cad5894
tharvik <tharvik@gmail.com>
2013-04-09 08:44:11 +0200

    Add variables to FlameMakerGUI

--- src/ch/epfl/flamemaker/gui/FlameMakerGUI.java
+++ src/ch/epfl/flamemaker/gui/FlameMakerGUI.java
@@ -1,5 +1,16 @@
 package ch.epfl.flamemaker.gui;
 
+import ch.epfl.flamemaker.color.Color;
+import ch.epfl.flamemaker.color.Palette;
+import ch.epfl.flamemaker.flame.Flame;
+import ch.epfl.flamemaker.geometry2d.Rectangle;
+
 public class FlameMakerGUI {
+	private Flame.Builder builder;
+	private Color background;
+	private Palette palette;
+	private Rectangle frame;
+	private int density;
 	
+
 }
