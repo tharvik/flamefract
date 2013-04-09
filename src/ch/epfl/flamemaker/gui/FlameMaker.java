c58bc8176032fbc00ac398c5b97d568b6a6229d2
glsubri <guylaurentsubri@gmail.com>
2013-04-09 08:42:17 +0200

    Added javadoc to FlameMaker.java

--- src/ch/epfl/flamemaker/gui/FlameMaker.java
+++ src/ch/epfl/flamemaker/gui/FlameMaker.java
@@ -1,8 +1,13 @@
 package ch.epfl.flamemaker.gui;
 
+/**
+ * Main class</br>
+ * Used to initiate the whole program, in
+ * particular the GUI.
+ */
 public class FlameMaker {
 	public static void main(String[] args) {
-		javax.swing.SwingUtilities.invokeLater(new Runnable()) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				new FlameMakerGUI().start();
 			}
